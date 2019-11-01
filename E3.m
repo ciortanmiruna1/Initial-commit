d4ec0a3294b03c08c6b42468870a3cf714a737dd
ciortanmiruna1 <57265272+ciortanmiruna1@users.noreply.github.com>
2019-11-01 19:25:25 +0200

    Add files via upload

---
+++ E3.m
@@ -0,0 +1,6 @@
+
+    real=randi([-10 10], 1, 3);
+    im=randi([-10 10], 1, 3)*i;
+    z=real+im
+    functie(real,im);
+    
---
+++ E41.m
@@ -0,0 +1,6 @@
+z = zeros(1,21);
+z(1,6)=1;
+n=0:20;
+m=-5:15;
+subplot(2,1,1), stem(n,z)
+subplot(2,1,2), stem(m,z)
