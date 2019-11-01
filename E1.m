606e63d72b72b00f9098a2870393f2224e0f9349
ciortanmiruna1 <57265272+ciortanmiruna1@users.noreply.github.com>
2019-11-01 19:22:22 +0200

    Add files via upload

---
+++ E1.m
@@ -0,0 +1,13 @@
+%E1.Ini?ializa?i �n MATLAB cei doi vectori ?i realiza?i �nmul?irea.
+%Ce opera?ie se efectueaz??
+
+a=[1,2,3,4,5];
+b=[1;1;1;1;1];
+%a) numarul de linii din b trebuie sa fie egal cu nr de coloane din a
+%   pentru ca inmultirea sa poate avea sens
+    a*b
+%   are loc inmultirea clasica a matricelor
+%b)
+    b*a
+%c)
+    a.*b
